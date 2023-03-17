a45ff96b886c8546c7a8fc0b40757d240ca309bf
samuelnovaiscavelho <samuel.novais247@gmail.com>
2023-03-16 22:13:52 -0300

    aplicação container

---
+++ src/main/java/com/example/projetocontainer/ProjetocontainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetocontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetocontainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetocontainerApplication.class, args);
+    }
+
+}
